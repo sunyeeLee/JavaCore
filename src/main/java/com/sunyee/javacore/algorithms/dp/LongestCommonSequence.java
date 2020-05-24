1063ae117deca385806a7aa9a8e057008d93e21b
sunyeeLee <949562384@qq.com>
2020-05-24 16:43:00 +0800

    feat(location): get longest common sequence from dp array

--- src/main/java/com/sunyee/javacore/algorithms/dp/LongestCommonSequence.java
+++ src/main/java/com/sunyee/javacore/algorithms/dp/LongestCommonSequence.java
@@ -64,10 +64,9 @@ public class LongestCommonSequence {
 
         for (int i =1; i< word1.length(); i++){
             for (int j=1; j< word2.length(); j++){
+                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 if (word1.charAt(i) == word2.charAt(j)){
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]) + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i-1][j-1], dp[i][j]) + 1;
                 }
             }
         }
@@ -75,10 +74,49 @@ public class LongestCommonSequence {
         return dp;
     }
 
+    /**
+     * dp矩阵中最右下角的值代表 str1整体和 str2整体的最长公共子序列的长度。通过整个 dp矩阵的状态，
+     * 可以得到最长公共子序列。具体方法如下：
+     * 1.从矩阵的右下角开始，有三种移动方式：向上、向左、向左上。
+     *      假设移动的过程中，i表示此时的行数，j表示此时的列数，同时用一个变量res来表示最长公共子序列。
+     * 2.如果dp[i][j]大于dp[i-1][j]和dp[i][j-1]，说明之前在计算dp[i][j]的时候，一定是选择了决策dp[i-1][j-1]+1，
+     *      可以确定 str1[i]等于 str2[j]，并且这个字符一定属于最长公共子序列，把这个字符放进res，然后向左上方移动。
+     * 3.如果 dp[i][j]等于 dp[i-1][j]，说明之前在计算 dp[i][j]的时候，dp[i-1][j-1]+1 这个决策不是必须选择的决策，
+     *      向上方移动即可。4.如果dp[i][j]等于dp[i][j-1]，与步骤3同理，向左方移动。5.如果dp[i][j]同时等于 dp[i-1][j]和dp[i][j-1]，
+     *      向上还是向下无所谓，选择其中一个即可，反正不会错过必须选择的字符。也就是说，通过dp求解最长公共子序列的过程就是还原出当时
+     *      如何求解dp的过程，来自哪个策略，就朝哪个方向移动.
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public static String lcs(String word1, String word2){
+        int[][] dp = getDp(word1, word2);
+        int m = word1.length()-1;
+        int n = word2.length()-1;
+        char[] lcs= new char[dp[m][n]];
+        int index = lcs.length-1;
+        while (index >= 0){
+            if (n > 0 && dp[m][n] == dp[m][n-1]){
+                n--;
+            } else if (m > 0 && dp[m][n] == dp[m-1][n]){
+                m--;
+            } else {
+                lcs[index--] = word1.charAt(m);
+                m--;
+                n--;
+            }
+        }
+        return String.valueOf(lcs);
+    }
+
     public static void main(String[] args) {
         String str1="1A2C3D4B56";
         String str2="B1D23CA45B6A";
         int[][] dp = getDp(str1, str2);
         System.out.println(Arrays.deepToString(dp));
+
+        // get lcs of dp
+        String lcs = lcs(str1, str2);
+        System.out.println(lcs);
     }
 }
